eb31bed293792d48157c01d3383d024ee7972348
Angel-O <Aorhymes@gmail.com>
2017-06-27 22:19:49 +0100

    added default files

---
+++ ProbabilityCalculator/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProbabilityCalculator.Startup))]
+namespace ProbabilityCalculator
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
